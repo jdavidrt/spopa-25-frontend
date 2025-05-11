394c9c6911771c58c0ce7e325b06c4c9e8435db3
davidrt <jdramirezt@unal.edu.co>
2025-05-11 15:05:25 -0500

    INITIAL COMMIT

---
+++ src/components/Hero.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import logo from "../assets/SPOPALogo.png";
+
+const Hero = () => (
+  <div className="text-center hero my-5">
+    <img className="mb-3 app-logo" src={logo} alt="React logo" width="120" />
+    <h1 className="mb-4">SPOPA</h1>
+  </div>
+);
+
+export default Hero;
