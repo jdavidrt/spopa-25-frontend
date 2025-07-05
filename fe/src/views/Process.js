b2b84edbbac63b06fda53a540f12f29672383d29
David Santiago Castañeda Venegas <dscastanedav@unal.edu.co>
2025-07-05 09:42:51 -0500

    tratando de arreglar procesos

--- fe/src/views/Process.js
+++ fe/src/views/Process.js
@@ -1,7 +1,7 @@
 // frontend/src/components/Procesos.js
 import React, { useEffect, useState } from "react";
 
-const API_URL = process.env.REACT_APP_PROCESS_API || "http://localhost:4000/api/process";
+const API_URL = process.env.REACT_APP_PROCESS_API || "http://localhost:3010/api/process";
 console.log("🪼🪼🪼🪼🪼🪼", API_URL)
 function Process() {
   const [process, setProcesos] = useState([]);
